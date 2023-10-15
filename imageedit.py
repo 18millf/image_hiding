5f667156049528cb749a67ef890a5daa54e20ced
Millzy <millzyyt08@gmail.com>
2023-10-15 17:50:03 +0100

    fixed using the wrong offset for data offset

--- imageedit.py
+++ imageedit.py
@@ -1,11 +1,11 @@
 FILE_HEADER_SIZE = 12
 
-##message = input("Message to hide: ")
+# message = input("Message to hide: ")
 message = "A SUPER SECRET MESSAGE"
 
 infile = open("LAND2.BMP", "rb")
 raw_bytes = infile.read()
-bytes = bytearray(raw_bytes)
+file_bytes = bytearray(raw_bytes)
 
 bin_message = bytearray(message, 'ascii')
 
@@ -13,14 +13,35 @@ bin_message = bytearray(message, 'ascii')
 # File Size 2-3
 # Reserved 4-7
 # Data Offset 8-B
-data_offset = int.from_bytes(bytes[8:11], "big")
+data_offset = int.from_bytes(file_bytes[0xA:0xD], "little")
 print(data_offset)
 
-new_offset = data_offset + len(message) + 4 # extra  bytes required for original offset
-new_offset_bytes = bytearray(new_offset, "big")
+print(len(file_bytes))
 
-bytes[8] = new_offset_bytes[0]
-bytes[9] = new_offset_bytes[1]
-bytes[10] = new_offset_bytes[2]
-bytes[10] = new_offset_bytes[3]
+new_offset = data_offset + len(message) + 4  # extra  bytes required for original offset
+print(new_offset)
 
+new_offset_bytes = new_offset.to_bytes(4, "little")
+
+old_bytes = file_bytes
+
+file_bytes[0xA] = new_offset_bytes[0]
+file_bytes[0xB] = new_offset_bytes[1]
+file_bytes[0xC] = new_offset_bytes[2]
+file_bytes[0xD] = new_offset_bytes[3]
+
+bin_message.extend(data_offset.to_bytes(4, "big"))
+
+i = data_offset
+while i != new_offset:
+    file_bytes.insert(i, bin_message[i - data_offset])
+    print(f"Inserted {bin_message[i - data_offset]} at {i}")
+    i = i + 1
+
+print(len(file_bytes))
+
+infile.close()
+
+outfile = open("OUTPUT.BMP", "wb")
+outfile.write(file_bytes)
+outfile.close()
